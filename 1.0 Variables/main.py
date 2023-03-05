fa42b69af4da6e1412e374557f47212ddf4ffb62
adesinna <shananatest@gmail.com>
2023-03-05 12:03:35 +0100

    variables assignments

---
+++ 1.0 Variables/main.py
@@ -0,0 +1,22 @@
+# This a comment in python, this line of code will be ignored.
+
+print('Hello World')  # this will print on the console
+
+# Strings:
+#   These are characters in "" or ''.
+
+print('My name is Adesina\nI live in Lagos')  # \n will let the string starts in a new line
+
+# Concatenation of strings
+
+print("I" + " " + "am" + " " + "Adesina")
+
+# To get strings from user, you use the input function
+
+print("Hello " + input("Enter your name:\n"))  # this will collect the username as a string
+
+# variables
+name = "Adesina"  # this is like a phone number being given a name in your phonebook
+
+
+
---
+++ 1.0 Variables/project.py
@@ -0,0 +1,8 @@
+# Create a band name generator using input function and variables
+
+name = input('Enter the name of favorite element:\n')
+
+pet = input('Enter your best football club:\n')
+
+band_name = name + " " + pet
+print(f'Your band name is "{band_name}"')
